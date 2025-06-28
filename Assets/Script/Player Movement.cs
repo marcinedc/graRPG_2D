be04cd37a354db84fac920f5a9a1477cc6c8ef72
marcinedc <marcin1357@vp.pl>
2025-06-29 00:39:41 +0200

    dodano animacje postaci

--- Assets/Script/Player Movement.cs
+++ Assets/Script/Player Movement.cs
@@ -8,11 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private PlayerMove playerMove;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake()
     {
         playerMove = new PlayerMove();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnEnable()
@@ -33,6 +35,10 @@ public class PlayerMovement : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerMove.Movement.Move.ReadValue<Vector2>();
+
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
     }
 
     private void Move()
